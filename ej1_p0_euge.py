47ad68c27cb26ba4e982ac83afae55a41721abb3
Eugenia DP <121134471+EugeniaDP@users.noreply.github.com>
2026-03-09 23:20:31 -0300

    Práctica 0, ej. 1 (f(x) = cos(x))

--- ej1_p0_euge.py
+++ ej1_p0_euge.py
@@ -1,4 +1,4 @@
-#3. Implementar en Python el proceso anterior, que use una función (externa 
+#1. Implementar en Python el proceso anterior, que use una función (externa 
 #implementada, o bien pasada como parámetro), que lea el x como parámetro y un n 
 #natural, y luego sobre el x inicial aplique la f n veces, finalmente devolviendo el resultado 
 #obtenido tras la última aplicación.
@@ -11,5 +11,6 @@ def mi_funcion(x0, n):
     x1 = cos(x0)
     print(x1)
     mi_funcion(x1, n-1)
+
 
 mi_funcion(1, 50)
