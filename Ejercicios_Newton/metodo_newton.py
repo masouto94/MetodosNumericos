4d77e2109f46790910186c6491c664667dc50270
Matias Souto <masouto94@gmail.com>
2026-03-16 19:35:34 -0300

    Agrego div para forzar error al dividir por cero

--- Ejercicios_Newton/metodo_newton.py
+++ Ejercicios_Newton/metodo_newton.py
@@ -5,6 +5,14 @@ import sympy
 # Se puede asegurar que existe un e > 0 para el que si tomamos cualquier x0 del intervalo (r-e, r+3)
 # Nos encontraremos con una sucesión que converge a r en xn
 
+
+# Fuerzo que al dividir por cero tire error
+def div(num, den):
+    if den.is_zero:
+        raise ZeroDivisionError("División por cero")
+    return num / den
+
+
 x = sympy.Symbol("x")
 func = x**2 - 2
 
@@ -13,14 +21,19 @@ def metodo_newton(f, x0, error):
     if f.subs(x, x0) == 0:
         return x0
     dx_f = sympy.diff(f)
-    x1 = x0 - (f.subs(x, x0) / dx_f.subs(x, x0))
-    while abs(x1 - x0) > error:
-        x0 = x1
-        x1 = x0 - (f.subs(x, x0) / dx_f.subs(x, x0))
-    return float(x1)
+    try:
+        x1 = x0 - div(f.subs(x, x0), dx_f.subs(x, x0))
+        while abs(x1 - x0) > error:
+            x0 = x1
+            x1 = x0 - div(f.subs(x, x0), dx_f.subs(x, x0))
+        return float(x1)
+    except ZeroDivisionError:
+        print(f"Error: f'(x) da 0 con x0 = {x0}")
+        return x0
 
 
 print(metodo_newton(func, 1, 0.001))
+print(metodo_newton(func, 0, 0.001))
 
 # Solo funciona si estamos "cerca" pero parece que anda igual pero tarda
 # print(metodo_newton(func, 10000, 0.001))
